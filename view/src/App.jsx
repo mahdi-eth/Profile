7a4a2edcc1ffa44aebcbe4cbc9c3de4968beca57
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2023-02-04 11:10:51 +0330

    Adding modal

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,69 +1,82 @@
-import { useState } from "react";
 import compLogo from "./assets/img/compLogo.jpg";
 import tempProfilePic from "./assets/img/man1.webp";
 import phoneSVG from "./assets/icons/phone.svg";
 import mailSVG from "./assets/icons/mail.svg";
 import webSVG from "./assets/icons/web.svg";
 import locSVG from "./assets/icons/loc.svg";
-import Modal from "./components/inputsModal";
+import { useState } from "react";
+import ModalPop from "./components/inputsModal";
 
 function App() {
-    // const openInputsModal = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [theme, setTheme] = useState("#d87093");
 
-    // }
+    console.log(showModal);
 
     return (
-        <div className="main-container line">
-            <div className="sec-line">
-                <div className="third-line">
-                    <div className="contaion display-flex justify-content-center">
-                        <div className="sub-container display-flex justify-content-center align-items-center">
-                            <div className="profile-holder display-flex align-items-center flex-coloumn special-m">
-                                <img
-                                    className="user z-index"
-                                    src={tempProfilePic}
-                                    alt="Profile picture"
-                                />
-                                <img
-                                    className="comp-logo border-radius-19 z-index"
-                                    src={compLogo}
-                                    alt="Company logo!"
-                                />
-                            </div>
-                            <div className="form-info display-flex align-items-center flex-coloumn z-index">
-                                <div className="form-info-header display-flex align-items-center flex-coloumn">
-                                    <h1 className="heading">
-                                        PHIL Janet anderson
-                                    </h1>
-                                    <h2 className="sales">
-                                        Sales Representative
-                                    </h2>
+        <>
+            <ModalPop
+                showModal={showModal}
+                setShowModal={setShowModal}
+                theme={theme}
+                setTheme={setTheme}
+            />
+            <div className="main-container line">
+                <div className="sec-line">
+                    <div className="third-line">
+                        <div className="contaion display-flex justify-content-center">
+                            <div className="sub-container display-flex justify-content-center align-items-center">
+                                <div className="profile-holder display-flex align-items-center flex-coloumn special-m">
+                                    <img
+                                        className="user z-index"
+                                        src={tempProfilePic}
+                                        alt="Profile picture"
+                                    />
+                                    <img
+                                        className="comp-logo border-radius-19 z-index"
+                                        src={compLogo}
+                                        alt="Company logo!"
+                                    />
                                 </div>
-                                <div className="form-info-items display-flex flex-coloumn">
-                                    <div>
-                                        <img src={phoneSVG} alt="phone" />
-                                        +123-456-789
+                                <div className="form-info display-flex align-items-center flex-coloumn z-index">
+                                    <div className="form-info-header display-flex align-items-center flex-coloumn">
+                                        <h1 className="heading">
+                                            PHIL Janet anderson
+                                        </h1>
+                                        <h2 className="sales">
+                                            Sales Representative
+                                        </h2>
                                     </div>
-                                    <div>
-                                        <img src={mailSVG} alt="mail" />
-                                        Hello@reallygreatsite.com
-                                    </div>
-                                    <div>
-                                        <img src={webSVG} alt="web" />
-                                        www.reallygreatsite.com
-                                    </div>
-                                    <div className="none-bottom-border">
-                                        <img src={locSVG} alt="location" />
-                                        123 AnyWhere st., Any City, st 1234
+                                    <div className="form-info-items display-flex flex-coloumn">
+                                        <div>
+                                            <img src={phoneSVG} alt="phone" />
+                                            +123-456-789
+                                        </div>
+                                        <div>
+                                            <img src={mailSVG} alt="mail" />
+                                            Hello@reallygreatsite.com
+                                        </div>
+                                        <div>
+                                            <img src={webSVG} alt="web" />
+                                            www.reallygreatsite.com
+                                        </div>
+                                        <div className="none-bottom-border">
+                                            <img src={locSVG} alt="location" />
+                                            123 AnyWhere st., Any City, st 1234
+                                        </div>
                                     </div>
                                 </div>
+                                <button
+                                    onClick={() => setShowModal(true)}
+                                    className="my-inputs z-index">
+                                    My Inputs
+                                </button>
                             </div>
-                            <Modal />
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- view/src/components/inputsModal/index.jsx
+++ view/src/components/inputsModal/index.jsx
@@ -1,16 +1,126 @@
-import React from "react";
 import { useState } from "react";
-const Modal = () => {
-    const [modalState, setModalState] = useState(false);
+import { HexColorPicker } from "react-colorful";
+import "./index.css";
 
-    showModal = (e) => {
-        setModalState(true);
+const ModalPop = ({ showModal, setShowModal, theme, setTheme }) => {
+    const [popOverPicker, setPopOverPicker] = useState(false);
+
+    const hideModal = (e) => {
+        if (e.target.className.includes("activated-modal") && !popOverPicker) {
+            setShowModal(false);
+        }
+        if (
+            !e.target.className.includes("hex-color-changer") &&
+            !e.target.className.includes("react-colorful") &&
+            popOverPicker
+        ) {
+            setPopOverPicker(false);
+        }
     };
+
+    const [userData, setUserDate] = useState({
+        username: "",
+        phoneNumber: "",
+        email: "",
+        website: "",
+        address: ""
+    });
+
+    const saveUser = () => {
+        console.log("save");
+    };
+
     return (
-        <button onClick={showModal} className="my-inputs z-index">
-            My Inputs
-        </button>
+        <div
+            onClick={hideModal}
+            className={
+                showModal ? "activated-modal " + "modal-page" : "modal-page"
+            }>
+            <div
+                className={
+                    "modal-container display-flex flex-coloumn align-items-center"
+                }>
+                <p className="choose-prefers">Choose Your Prefers</p>
+                <div className="user-form-container display-flex flex-coloumn">
+                    <div>
+                        <p className="theme-color-text">Theme Color</p>
+                        <button
+                            className="theme-color"
+                            style={{ background: theme }}
+                            onClick={() => {
+                                setPopOverPicker(!popOverPicker);
+                            }}
+                        />
+                        {popOverPicker && (
+                            <div className="hex-color-changer">
+                                <HexColorPicker
+                                    color={theme}
+                                    onChange={setTheme}
+                                />
+                            </div>
+                        )}
+                    </div>
+                    <form className="inputs display-flex flex-coloumn">
+                        <input
+                            style={{ borderColor: theme + "80" }}
+                            placeholder="Username"
+                            name="Username"
+                            type="text"
+                        />
+                        <input
+                            style={{ borderColor: theme + "80" }}
+                            placeholder="Phone number"
+                            name="Phone number"
+                            type="text"
+                        />
+                        <input
+                            style={{ borderColor: theme + "80" }}
+                            placeholder="Email"
+                            name="Email"
+                            type="email"
+                        />
+                        <input
+                            style={{ borderColor: theme + "80" }}
+                            placeholder="Website"
+                            name="Website"
+                            type="text"
+                        />
+                        <input
+                            style={{ borderColor: theme + "80" }}
+                            placeholder="Address"
+                            name="Address"
+                            type="text"
+                        />
+                        <div className="display-flex">
+                            <figure>
+                                <img
+                                    className="first-profile z-index-6"
+                                    src={testProfile}
+                                    alt="firstIcon"
+                                />
+                                <Radio name="a" shape="round" color="primary" />
+                            </figure>
+                            <figure>
+                                <img
+                                    className="first-profile z-index-6"
+                                    src={test02Profile}
+                                    alt="firstIcon"
+                                />
+                                <Radio name="a" shape="round" color="primary" />
+                            </figure>
+                        </div>
+                    </form>
+                    <button
+                        onClick={saveUser}
+                        style={{ background: theme }}
+                        type="submit"
+                        className="save-btn">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 };
 
-export default Modal;
+export default ModalPop;
